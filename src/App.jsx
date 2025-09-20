d84e7bfc85c657724e0ed77a99cd91bd3c3c4cf1
Caio Alves <caio.silva209@fatec.sp.gov.br>
2025-09-20 15:13:04 -0300

    primeiro commit do projeto, Caio Alves da Silva RA:2040482323012

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import LembreteEntrada from "./LembreteEntrada";
+
+function App() {
+  return (
+    <div className="container">
+      <div className= "row">
+        <div className="col-12">
+          "Hello, Lembretes"
+          <LembreteEntrada />
+        </div>
+      </div>
+    </div>
+  )
+}
+export default App;
---
+++ src/LembreteEntrada.jsx
@@ -0,0 +1,9 @@
+const LembreteEntrada = () => {
+    return (
+      <div>
+        <input type="text" className="form-control" placeholder="Digite seu novo lembrete" />
+        <button className="btn btn-primary mt-2">OK</button>
+      </div>  
+    );
+}
+export default LembreteEntrada;
