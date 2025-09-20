0f7b701cf22344a2be1c8a8e4d89ff69cef1dea9
Caio Alves <caio.silva209@fatec.sp.gov.br>
2025-09-20 18:46:36 -0300

    feat(p1): refatora App para componente de classe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import React from "react";
 import LembreteEntrada from "./LembreteEntrada";
 
-function App() {
-  return (
-    <div className="container">
-      <div className= "row">
-        <div className="col-12">
-          "Hello, Lembretes"
-          <LembreteEntrada />
+class App extends React.Component {
+  render(){
+    return (
+      <div className="container">
+        <div className= "row">
+          <div className="col-12">
+            <h1 className="display-5 text-center">Lembretes</h1>
+            <LembreteEntrada />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 export default App;
